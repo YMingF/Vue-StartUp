403c9eec40091f1448ccc42b5b2d3f46bf6b0ef3
YM <a834319313@163.con>
2021-09-09 13:38:24 +0800

    vue起手式代码

---
+++ src/demo.vue
@@ -0,0 +1,31 @@
+
+<!--视图-->
+<template>
+    <div class="red">
+        {{n}}
+        <button @click="add">+1</button>
+    </div>
+</template>
+
+<!--视图以外的其它选项-->
+<script>
+export default{
+    data() {
+        return {
+            n:0
+        }
+    },
+    methods:{
+        add(){
+            this.n+=1
+        }
+    }
+}
+</script>
+
+<style scoped>
+.red{
+    color:red
+}
+</style>
+<!--vue-loader可以将这一整串代码变成一个对象-->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from "vue";
+// import App from "./App.vue";
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false;
 
+import Demo from "./demo.vue";
+console.log(Demo);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app", //对id为app的标签进行MVC的封装
+  render(h) {
+    //h用于创建元素
+    return h(Demo);
+  },
+});
